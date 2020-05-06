0c031914ff8a6d75c244bfc29ebe0b7887476a7b
Paulyn Monasterio <filipinaa93@gmail.com>
2020-05-06 18:33:32 -0400

    Updates

--- precache-manifest.39ef68022614d0e2ecc97573ce4b44f0.js
+++ precache-manifest.39ef68022614d0e2ecc97573ce4b44f0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41bd75b7d2dbfda1d42828d3219a5f40",
+    "revision": "58a546a48c8129029c91e234c8ed17d3",
     "url": "/monster-rolodex/index.html"
   },
   {
-    "revision": "496d4cef4f69a56c4dbc",
+    "revision": "9c3ef76df7a5f656f232",
     "url": "/monster-rolodex/static/css/main.6ce3da0d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "496d4cef4f69a56c4dbc",
-    "url": "/monster-rolodex/static/js/main.8f9c4e80.chunk.js"
+    "revision": "9c3ef76df7a5f656f232",
+    "url": "/monster-rolodex/static/js/main.b486dcc8.chunk.js"
   },
   {
     "revision": "90506cc279e62bb14608",
