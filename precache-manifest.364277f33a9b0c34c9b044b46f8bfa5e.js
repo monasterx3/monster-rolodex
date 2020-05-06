4c48a61381c5546403f0996e12dfaf35fb21d39e
Paulyn Monasterio <filipinaa93@gmail.com>
2020-05-06 18:20:00 -0400

    Updates

---
+++ precache-manifest.364277f33a9b0c34c9b044b46f8bfa5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90a53752fd7f7b4280544b658150b57",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e232baf41d2186600f2a",
+    "url": "/monster-rolodex/static/css/main.b2ba5eec.chunk.css"
+  },
+  {
+    "revision": "d9fa166f4706a29f5ddc",
+    "url": "/monster-rolodex/static/js/2.9c067670.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9c067670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e232baf41d2186600f2a",
+    "url": "/monster-rolodex/static/js/main.caaaf99e.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  }
+]);
